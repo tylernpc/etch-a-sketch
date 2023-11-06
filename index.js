4555b2a9a45480aa123ab39830ecd848d1bff07c
tylernpc <103677076+tylernpc@users.noreply.github.com>
2023-11-05 23:32:52 -0600

    Inital Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// for (let i = 0; i < 16; i++) {
+//   const gridPiece = document.createElement("div");
+//   gridPiece.innerText = "div";
+//   document.body.appendChild(gridPiece);
+// }
+
+for (i = 0; i < yield.length; i++) {
+    y[i].style.flex = "1";
+}
