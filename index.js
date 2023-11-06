41d04b88d72f4a601644979680129049b95bbfc6
tylernpc <103677076+tylernpc@users.noreply.github.com>
2023-11-06 09:21:53 -0600

    Added more to JS, solid amount isn't correct or going to be used but may potentially contain an answer

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+// set the constants that will become physical on the html tab
+const container = document.getElementById("container");
+let rows = document.getElementById("gridRow");
+let cells = document.getElementById("cell");
+
 // for (let i = 0; i < 16; i++) {
 //   const gridPiece = document.createElement("div");
 //   gridPiece.innerText = "div";
 //   document.body.appendChild(gridPiece);
 // }
 
-for (i = 0; i < yield.length; i++) {
-    y[i].style.flex = "1";
-}
+// function theGrid () {
+//     document.createElement("div");
+
+// }
+
+// for (i = 0; i < yield.length; i++) {
+//     y[i].style.flex = "1";
+// }
